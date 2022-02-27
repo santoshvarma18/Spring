81c5abb8072b83bac521c930463e8287e146cf66
Mutyala Santosh <santoshmutyala@zemosolabs.com>
2022-02-27 23:28:31 +0530

    Using drop down list without hardcoding

--- src/main/java/com/luv2code/springdemo/mvc/Student.java
+++ src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -1,13 +1,27 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
 
+    private LinkedHashMap<String, String> countryOptions;
+
 
     public Student(){
+
+        // populate country options: usd ISO country code
+
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR","Brazil");
+        countryOptions.put("FR","France");
+        countryOptions.put("DE","Germany");
+        countryOptions.put("IN","India");
+        countryOptions.put("US","United States of America");
 
     }
 
@@ -34,4 +48,9 @@ public class Student {
     public void setCountry(String country) {
         this.country = country;
     }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
+
 }
