a8e6f799e897e0b741f1586593f5a9cbe7c75240
Mutyala Santosh <santoshmutyala@zemosolabs.com>
2022-02-27 23:05:45 +0530

    Usage of text fields in spring

---
+++ src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo.mvc;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    public Student(){
+
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
